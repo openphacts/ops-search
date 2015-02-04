eaac5438b74411a2cb780df4086e9510e9d211ca
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-02-04 17:04:18 +0000

    Refactor to use class

--- populate_elasticsearch.py
+++ populate_elasticsearch.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
-import json
+import sys
 import time
-try:
-    import ijson.backends.yajl2 as ijson
-except ex:
-    print("Can't find yajl2, JSON parsing will be slower")
-    import ijson
-
-import yaml
+import json
 from urllib.request import FancyURLopener
 from urllib.parse import urljoin, quote, urlencode, urlencode, urlencode, urlencode
 
+import yaml
+
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+try:
+    import ijson.backends.yajl2 as ijson
+except ex:
+    print("Can't find py-yajl, JSON parsing will be slower")
+    import ijson
+
 REPORT_EVERY=1200
+DEFAULT_SPARQL_TIMEOUT=60
 
-
+## TODO: Parameterize the config
 with open("config.yaml") as f:
     conf = yaml.load(f)
-
 
 def uri_to_qname(uri):
     for p,u in conf.get("prefixes", {}).items():
@@ -32,108 +35,87 @@ def sparql_prefixes():
         sparql.append("PREFIX %s: <%s>" % (p,u))
     return "\n".join(sparql)
 
-def binding_as_doc(node):
-    global count, lastCheck
-    # now, assume it's in sparql JSON format, e.g.
-#{
-#    "substanceType": { "type": "uri" , "value": "http://rdf.ebi.ac.uk/terms/chembl#Antibody" } ,
-#    "substance": { "type": "uri" , "value": "http://rdf.ebi.ac.uk/resource/chembl/molecule/CHEMBL1201579" } ,
-#    "label": { "type": "literal" , "value": "CAPROMAB PENDETIDE" } ,
-#    "prefLabel": { "type": "literal" , "value": "CAPROMAB PENDETIDE" } ,
-#    "altLabel": { "type": "literal" , "value": "CAPROMAB PENDETIDE" }
-#}
-    count += 1
-    if (count % REPORT_EVERY == 0):
-        now = time.time()
-        speed = REPORT_EVERY/(now-lastCheck)
-        avgSpeed = count/(now-start)
-        print("n=%s, speed=%0.1f/sec, avgSpeed=%0.1f/sec" % (count, speed, avgSpeed))
-        lastCheck = now
-    uri = node["substance"]["value"]
-    substanceType = uri_to_qname(node["substanceType"]["value"])
-    label = node["label"]["value"]
-    prefLabel = node["label"]["value"]
-    if not "altLabel" in node:
-    #    print("Straaaaaaaaaange", node)
-        altLabel = None
-    else:
-        altLabel = node["altLabel"]["value"]
-    if uri not in altLabels:
-        body = {
-                            "@id": uri,
-                            "@type": [substanceType, "chembl:Substance"],
-                            "label": label,
-                            "prefLabel": prefLabel,
-                            }
-        if altLabel:
-            body["altLabel"] = [altLabel]
-        msg = {
-                    "_index": "chembl19",
-                    "_type": "substance",
-                    "_id": uri,
-                    "_source": body
-                }
-        altLabels[uri] = set([altLabel])
-        return msg
-    else:
-        labels = altLabels[uri]
-        if altLabel in labels:
-                print("Something else changed", uri)
-                return None # skip
-        labels.add(altLabel)
-        msg = {
-            "_op_type": "update",
-            "_index": "chembl19",
-            "_type": "substance",
-            "_id": uri,
-            "doc": {
-              "altLabel": list(labels)
-              }
-            }
-        return msg
 
-
-class Populater:
+class Indexer:
 
     es_hosts = conf.get("elasticsearch", None)
     es = Elasticsearch(es_hosts)
 
     urlOpener = FancyURLopener()
-    urlOpener.addheader("Accept", "application/sparql-results+json, applicaton/json;q=0.1")
+    urlOpener.addheader("Accept",
+        "application/sparql-results+json, applicaton/json;q=0.1")
 
     def __init__(self, index, doc_type):
         self.index = index
         self.doc_type = doc_type
         self.conf = conf["indexes"][index][doc_type]
-        self.added = set()
-        # for stats
+        self.indexed = set()
+        self.reset_stats()
+        self.properties = {}
+
+    def reset_stats(self):
         self.count = 0
         self.start = time.time()
-        self.lastCheck = start
+        self.lastCheck = self.start
 
-    def sparql():
+    def sparql_property(self, prop):
+        short = prop.split(":")[-1]
+        if short not in self.properties:
+            name = short
+        else:
+            name = prop.replace(":", "_")
+            ## FIXME: Any other illegal chars for SPARQL VARNAME?
+            # http://www.w3.org/TR/sparql11-query/#rVARNAME
+        self.properties[name] = prop
+
+    def sparql(self):
         sparql = []
         sparql.append(sparql_prefixes())
         sparql.append("SELECT *")
         sparql.append("WHERE {")
         if "graph" in self.conf:
-            sparql.append(" GRAPH <%s>" % self.conf["graph"])
+            sparql.append(" GRAPH <%s> {" % self.conf["graph"])
 
-        ## clever bit here
+        optionals = False
+        if "type" in self.conf:
+            sparql.append("   ?uri a %s ." % self.conf["type"])
+            optionals = True
+        else:
+            sparql.append("   ?uri ")
+
+        properties = []
+
+        if "common_properties" in conf:
+            properties.extend(conf["common_properties"])
+
+        if "properties" in self.conf:
+            properties.extend(self.conf["properties"])
+
+        if not properties:
+            raise Exception("No properties for %s %s" % (self.index, self.doc_type))
+
+        print("Indexing properties ", properties)
+        sparql.extend(map(self.sparql_property, properties))
 
         if "graph" in self.conf:
-            sparl.append(" }")
+            sparql.append(" }")
         sparql.append("}")
+        if "limit" in conf["sparql"]:
+            sparql.append("LIMIT %s" % conf["sparql"]["limit"])
 
+        sparqlStr = "\n".join(sparql)
+        print(sparqlStr)
+        return sparqlStr
 
-    def sparqlUrl(self):
+    def sparqlURL(self):
         return urljoin(conf["sparql"]["uri"],
             "?" + urlencode(dict(query=self.sparql(),
-                                timeout=conf["sparql"].get("timeout_s", 300))))
-
+                            timeout=conf["sparql"].get("timeout_s",
+                                    DEFAULT_SPARQL_TIMEOUT))))
 
     def json_reader(self):
-        with self.urlOpener.open(SPARQL) as jsonFile:
+        url = self.sparqlURL()
+        with self.urlOpener.open(url) as jsonFile:
         #with open("substance.json", mode="rb") as jsonFile:
             bindings = ijson.items(jsonFile, "results.bindings.item")
             for binding in bindings:
@@ -142,15 +124,58 @@ class Populater:
                 if n is not None:
                     yield n
 
+    def binding_as_doc(self, node):
+        # now, assume it's in sparql JSON format, e.g.
+    #{
+    #    "substanceType": { "type": "uri" , "value": "http://rdf.ebi.ac.uk/terms/chembl#Antibody" } ,
+    #    "substance": { "type": "uri" , "value": "http://rdf.ebi.ac.uk/resource/chembl/molecule/CHEMBL1201579" } ,
+    #    "label": { "type": "literal" , "value": "CAPROMAB PENDETIDE" } ,
+    #    "prefLabel": { "type": "literal" , "value": "CAPROMAB PENDETIDE" } ,
+    #    "altLabel": { "type": "literal" , "value": "CAPROMAB PENDETIDE" }
+    #}
+        self.count += 1
+        if (self.count % REPORT_EVERY == 0):
+            now = time.time()
+            speed = REPORT_EVERY/(now-self.lastCheck)
+            avgSpeed = self.count/(now-self.start)
+            print("n=%s, speed=%0.1f/sec, avgSpeed=%0.1f/sec" % (count, speed, avgSpeed))
+            self.lastCheck = now
+        uri = node["substance"]["value"]
+        substanceType = uri_to_qname(node["substanceType"]["value"])
 
-for index in conf["indexes"]:
-    try:
+        if uri not in altLabels:
+            body = {
+                                "@id": uri,
+                                "@type": [substanceType, "chembl:Substance"],
+                                "label": label,
+                                "prefLabel": prefLabel,
+                                }
+            if altLabel:
+                body["altLabel"] = [altLabel]
+            msg = {
+                        "_index": "chembl19",
+                        "_type": "substance",
+                        "_id": uri,
+                        "_source": body
+                    }
+            altLabels[uri] = set([altLabel])
+            return msg
+
+    def load(self):
+        self.reset_stats()
+        bulk(self.es, self.json_reader())
+
+def main(*args):
+
+    for index in conf["indexes"]:
+        try:
             es.indices.delete(index=index)
-    except:
+        except:
             pass
+        for doc_type in conf["indexes"][index]:
+            indexer = Indexer(index, doc_type)
+            indexer.load()
 
 
-
-
-#bulk(es, json_reader())
-#altLabels = {}
+if __name__ == "__main__":
+    main(*sys.argv[1:])
